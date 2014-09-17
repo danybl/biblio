41243cdfca1ed720080350d16226b5d85c53d158
Cedric Soumpholphakdy <cedric_soum@hotmail.ca>
2014-09-17 14:39:31 -0400

    LivreDto ajoute get set

--- bibliotheque - 2/src/ca/qc/collegeahuntsic/bibliotheque/dto/LivreDTO.java
+++ bibliotheque - 2/src/ca/qc/collegeahuntsic/bibliotheque/dto/LivreDTO.java
@@ -1,24 +1,71 @@
-
 package ca.qc.collegeahuntsic.bibliotheque.dto;
 
 import java.sql.Date;
 
 /**
  * Permet de représenter un tuple de la table livre.
- *
+ * 
  */
 
-public class LivreDTO {
+public class LivreDTO extends Dto {
 
-    public int idLivre;
+	private int idLivre;
 
-    public String titre;
+	public int getIdLivre() {
+		return this.idLivre;
+	}
 
-    public String auteur;
+	public void setIdLivre(int idLivre) {
+		this.idLivre = idLivre;
+	}
 
-    public Date dateAcquisition;
+	public String getTitre() {
+		return this.titre;
+	}
 
-    public int idMembre;
+	public void setTitre(String titre) {
+		this.titre = titre;
+	}
 
-    public Date datePret;
+	public String getAuteur() {
+		return this.auteur;
+	}
+
+	public void setAuteur(String auteur) {
+		this.auteur = auteur;
+	}
+
+	public Date getDateAcquisition() {
+		return this.dateAcquisition;
+	}
+
+	public void setDateAcquisition(Date dateAcquisition) {
+		this.dateAcquisition = dateAcquisition;
+	}
+
+	public int getIdMembre() {
+		return this.idMembre;
+	}
+
+	public void setIdMembre(int idMembre) {
+		this.idMembre = idMembre;
+	}
+
+	public Date getDatePret() {
+		return this.datePret;
+	}
+
+	public void setDatePret(Date datePret) {
+		this.datePret = datePret;
+	}
+
+	private String titre;
+
+	private String auteur;
+
+	private Date dateAcquisition;
+
+	private int idMembre;
+
+	private Date datePret;
 }
