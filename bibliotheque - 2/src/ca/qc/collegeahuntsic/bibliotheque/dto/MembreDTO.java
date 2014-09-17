10961340bc8d77faeb5ff16048ad3f8972645fe6
JaskaranD <d.j.singh@hotmail.com>
2014-09-17 14:13:38 -0400

    Jaskaran Class MembreDTO

--- bibliotheque - 2/src/ca/qc/collegeahuntsic/bibliotheque/dto/MembreDTO.java
+++ bibliotheque - 2/src/ca/qc/collegeahuntsic/bibliotheque/dto/MembreDTO.java
@@ -6,15 +6,57 @@ package ca.qc.collegeahuntsic.bibliotheque.dto;
  *
  */
 
-public class MembreDTO {
+public class MembreDTO extends Dto {
 
-    public int idMembre;
+    private static final long serialVersionUID = 1L;
 
-    public String nom;
+    public int getIdMembre() {
+        return this.idMembre;
+    }
 
-    public long telephone;
+    public void setIdMembre(int idMembre) {
+        this.idMembre = idMembre;
+    }
 
-    public int limitePret;
+    public String getNom() {
+        return this.nom;
+    }
 
-    public int nbPret;
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public long getTelephone() {
+        return this.telephone;
+    }
+
+    public void setTelephone(long telephone) {
+        this.telephone = telephone;
+    }
+
+    public int getLimitePret() {
+        return this.limitePret;
+    }
+
+    public void setLimitePret(int limitePret) {
+        this.limitePret = limitePret;
+    }
+
+    public int getNbPret() {
+        return this.nbPret;
+    }
+
+    public void setNbPret(int nbPret) {
+        this.nbPret = nbPret;
+    }
+
+    private int idMembre;
+
+    private String nom;
+
+    private long telephone;
+
+    private int limitePret;
+
+    private int nbPret;
 }
