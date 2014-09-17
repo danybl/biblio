9152688f332254e2a21dcfdb70135bd683818430
Dany Benoit Lafond <dany.benoit.lafond@gmail.com>
2014-09-17 16:12:21 -0400

    membre DAO ajout de sgtring sql

--- bibliotheque - 2/src/ca/qc/collegeahuntsic/bibliotheque/dao/MembreDAO.java
+++ bibliotheque - 2/src/ca/qc/collegeahuntsic/bibliotheque/dao/MembreDAO.java
@@ -3,6 +3,30 @@ package ca.qc.collegeahuntsic.bibliotheque.dao;
 
 public class MembreDAO {
 
+    public static final String INSERT_REQUEST = "INSERT INTO membre (idMembre, nom, telephone, limitePret, nbpret)"
+        + "VALUES (?,?,?,?,?)";
+
+    public static final String UPDATE_INFO_REQUEST = "UPDATE membre"
+        + "SET nom = ?, telephone = ?, limitePret = ?, "
+        + "WHERE idMembre = ?";
+
+    public static final String DELETE_REQUEST = "DELETE FROM membre"
+        + "WHERE idMembre = ?";
+
+    public static final String GET_ALL_REQUEST = "SELECT idMembre, nom, telephone, limitePret, nbpret"
+        + " FROM membre";
+
+    //a completer
+    public static final String FIND_BY_ID = "";
+
+    public static final String FIND_BY_NOM = "SELECT idMembre, nom, telephone, limitePret, nbpret"
+        + "FROM membre"
+        + "where nom like ?";
+
+    public static final String FIND_BY_TEL = "SELECT idMembre, nom, telephone, limitePret, nbpret"
+        + "FROM membre"
+        + "where telephone = ?";
+
     public MembreDAO() {
         // TODO Auto-generated constructor stub
     }
