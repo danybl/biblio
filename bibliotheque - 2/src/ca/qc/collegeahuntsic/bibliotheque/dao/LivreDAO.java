62c2500dcb282de24a3e3942aace1c9a1fbc00c1
franz <http://download.eclipse.org/egit/updates>
2014-09-17 16:17:14 -0400

    LivreDAO ajout commentaire String SQL

--- bibliotheque - 2/src/ca/qc/collegeahuntsic/bibliotheque/dao/LivreDAO.java
+++ bibliotheque - 2/src/ca/qc/collegeahuntsic/bibliotheque/dao/LivreDAO.java
@@ -3,8 +3,15 @@ package ca.qc.collegeahuntsic.bibliotheque.dao;
 
 public class LivreDAO {
 
-    public LivreDAO() {
-        // TODO Auto-generated constructor stub
-    }
-
+    //    private static final String FIND_BY_LIVRE_ID = "";
+    //    private static final String DELETE_REQUEST = "DELETE FROM livre"
+    //    +"WHERE idLivre = ?";
+    //
+    //    private static final String GET_ALL_REQUEST = "SELECT idLivre, titre, auteur,dateAcquisition,idMembre, datePret"
+    //    +"FROM Livre";
+    //
+    //    private static final String FIND_BY_TITRE = "SELECT * FROM LIVRE WHERE livre LIKE '?'";
+    //    private static final String FIND_BY_MEMBRE = "SELECT * FROM LIVRE WHERE livre LIKE '?'";
+    //    private static final String EMPRUNT_REQUEST = "";
+    //    private static final String RETOUR_REQUEST = "";
 }
