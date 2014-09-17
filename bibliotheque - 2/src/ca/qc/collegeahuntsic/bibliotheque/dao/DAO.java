e981be46db99f0417114eae271b88daf6506526e
Dany Benoit Lafond <dany.benoit.lafond@gmail.com>
2014-09-17 13:46:36 -0400

    ajout classe dao

---
+++ bibliotheque - 2/src/ca/qc/collegeahuntsic/bibliotheque/dao/DAO.java
@@ -0,0 +1,14 @@
+
+package ca.qc.collegeahuntsic.bibliotheque.dao;
+
+import java.io.Serializable;
+
+public class DAO implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    public DAO() {
+        // TODO Auto-generated constructor stub
+    }
+
+}
