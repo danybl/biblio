d880f5fbb6426b0134b62c491658238b29089b14
David Gallego <davidgallego0125@gmail.com>
2014-09-17 16:17:48 -0400

    ajout private members class reservationDAO

--- bibliotheque - 2/src/ca/qc/collegeahuntsic/bibliotheque/dao/ReservationDAO.java
+++ bibliotheque - 2/src/ca/qc/collegeahuntsic/bibliotheque/dao/ReservationDAO.java
@@ -2,9 +2,27 @@
 package ca.qc.collegeahuntsic.bibliotheque.dao;
 
 public class ReservationDAO {
-
+    //Region private final members
+    //    private static final String FIND_RESERVATION_BY_ID = "select idReservation, idLivre, idMembre, dateReservation "
+    //        + "from reservation "
+    //        + "where idReservation = ?";
+    //
+    //    private static final String FIND_LIVRE_BY_ID = "select idReservation, idLivre, idMembre, dateReservation "
+    //        + "from reservation "
+    //        + "where idLivre = ? ";
+    //
+    //    private static final String FIND_MEMBRE_BY_ID = "select idReservation, idLivre, idMembre, dateReservation "
+    //        + "from reservation "
+    //        + "where idMembre = ? ";
+    //
+    //    private static final String INSERT_REQUEST = "insert into reservation (idReservation, idlivre, idMembre, dateReservation) "
+    //        + "values (?,?,?,to_date(?,'YYYY-MM-DD'))";
+    //
+    //    private static final String DELETE_REQUEST = "delete from reservation "
+    //        + "where idReservation = ?";
+    //EndRegion
     public ReservationDAO() {
-        // TODO Auto-generated constructor stub
+
     }
 
 }
