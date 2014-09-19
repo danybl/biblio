ed7b772ad33bf14bd8f17efda6bcfbede0c5995f
Dany Benoit Lafond <dany.benoit.lafond@gmail.com>
2014-09-19 15:58:25 -0400

    ajout de bibliothequeCreateur

---
+++ bibliotheque - 2/src/ca/qc/collegeahuntsic/bibliotheque/util/BibliothequeCreateur.java
@@ -0,0 +1,10 @@
+
+package ca.qc.collegeahuntsic.bibliotheque.util;
+
+public class BibliothequeCreateur {
+
+    public BibliothequeCreateur() {
+        // TODO Auto-generated constructor stub
+    }
+
+}
