a6b7eee27c2391064d2d4ccea7cec6c884b32bd4
Cedric_2 <Cedric_2@ASUSM11>
2014-09-20 14:41:40 -0500

    Classe ReservationService

---
+++ bibliotheque - 2/src/ca/qc/collegeahuntsic/bibliotheque/service/ReservationService.java
@@ -0,0 +1,11 @@
+
+package ca.qc.collegeahuntsic.bibliotheque.service;
+
+public class ReservationService {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
