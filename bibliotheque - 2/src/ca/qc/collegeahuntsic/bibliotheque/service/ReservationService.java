123ae4ee28baf5d07bb41a2b3b4d3f0832c4c1ae
Cedric_2 <Cedric_2@ASUSM11>
2014-09-20 15:55:07 -0500

    Set et Get rajouter, attribut en private.

--- bibliotheque - 2/src/ca/qc/collegeahuntsic/bibliotheque/service/ReservationService.java
+++ bibliotheque - 2/src/ca/qc/collegeahuntsic/bibliotheque/service/ReservationService.java
@@ -1,11 +1,28 @@
 
 package ca.qc.collegeahuntsic.bibliotheque.service;
 
-public class ReservationService {
+import ca.qc.collegeahuntsic.bibliotheque.dao.ReservationDAO;
 
-    public static void main(String[] args) {
-        // TODO Auto-generated method stub
+public class ReservationService extends Service {
 
+    /**
+     * TODO Auto-generated field javadoc
+     */
+
+    private static final long serialVersionUID = 1L;
+
+    private ReservationDAO reservationDAO;
+
+    public ReservationService() {
+        // TODO Auto-generated constructor stub
+    }
+
+    public ReservationDAO getReservationDAO() {
+        return this.reservationDAO;
+    }
+
+    public void setReservationDAO(ReservationDAO reservationDAO) {
+        this.reservationDAO = reservationDAO;
     }
 
 }
