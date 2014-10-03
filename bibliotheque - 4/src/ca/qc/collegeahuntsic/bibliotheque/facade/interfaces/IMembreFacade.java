a4edac6774e2b3190d0fe17877e1a589f30193c9
JaskaranD <d.j.singh@hotmail.com>
2014-10-03 17:15:11 -0400

    les interfaces du facade IMembreFacade et IPretFacade

--- bibliotheque - 4/src/ca/qc/collegeahuntsic/bibliotheque/facade/interfaces/IMembreFacade.java
+++ bibliotheque - 4/src/ca/qc/collegeahuntsic/bibliotheque/facade/interfaces/IMembreFacade.java
@@ -1,6 +1,34 @@
 
 package ca.qc.collegeahuntsic.bibliotheque.facade.interfaces;
 
+import ca.qc.collegeahuntsic.bibliotheque.db.Connexion;
+import ca.qc.collegeahuntsic.bibliotheque.dto.MembreDTO;
+import ca.qc.collegeahuntsic.bibliotheque.exception.dao.InvalidHibernateSessionException;
+import ca.qc.collegeahuntsic.bibliotheque.exception.dao.InvalidPrimaryKeyException;
+import ca.qc.collegeahuntsic.bibliotheque.exception.dto.DuplicateDTOException;
+import ca.qc.collegeahuntsic.bibliotheque.exception.dto.InvalidDTOClassException;
+import ca.qc.collegeahuntsic.bibliotheque.exception.dto.InvalidDTOException;
+import ca.qc.collegeahuntsic.bibliotheque.exception.dto.MissingDTOException;
+import ca.qc.collegeahuntsic.bibliotheque.exception.facade.FacadeException;
+import ca.qc.collegeahuntsic.bibliotheque.exception.service.ExistingLoanException;
+
 public interface IMembreFacade extends IFacade {
-    //
+
+    void ajouter(Connexion connexion,
+        MembreDTO membreDTO) throws InvalidHibernateSessionException,
+        InvalidDTOException,
+        InvalidPrimaryKeyException,
+        DuplicateDTOException,
+        MissingDTOException,
+        InvalidDTOClassException,
+        FacadeException;
+
+    void retirer(Connexion connexion,
+        MembreDTO membreDTO) throws InvalidHibernateSessionException,
+        InvalidDTOException,
+        InvalidPrimaryKeyException,
+        MissingDTOException,
+        ExistingLoanException,
+        InvalidDTOClassException,
+        FacadeException;
 }
--- bibliotheque - 4/src/ca/qc/collegeahuntsic/bibliotheque/facade/interfaces/IPretFacade.java
+++ bibliotheque - 4/src/ca/qc/collegeahuntsic/bibliotheque/facade/interfaces/IPretFacade.java
@@ -1,6 +1,45 @@
 
 package ca.qc.collegeahuntsic.bibliotheque.facade.interfaces;
 
+import ca.qc.collegeahuntsic.bibliotheque.db.Connexion;
+import ca.qc.collegeahuntsic.bibliotheque.dto.PretDTO;
+import ca.qc.collegeahuntsic.bibliotheque.exception.dao.InvalidCriterionException;
+import ca.qc.collegeahuntsic.bibliotheque.exception.dao.InvalidHibernateSessionException;
+import ca.qc.collegeahuntsic.bibliotheque.exception.dao.InvalidPrimaryKeyException;
+import ca.qc.collegeahuntsic.bibliotheque.exception.dao.InvalidPrimaryKeyRequestException;
+import ca.qc.collegeahuntsic.bibliotheque.exception.dao.InvalidSortByPropertyException;
+import ca.qc.collegeahuntsic.bibliotheque.exception.dto.InvalidDTOClassException;
+import ca.qc.collegeahuntsic.bibliotheque.exception.dto.InvalidDTOException;
+import ca.qc.collegeahuntsic.bibliotheque.exception.dto.MissingDTOException;
+import ca.qc.collegeahuntsic.bibliotheque.exception.facade.FacadeException;
+import ca.qc.collegeahuntsic.bibliotheque.exception.service.ExistingLoanException;
+import ca.qc.collegeahuntsic.bibliotheque.exception.service.ExistingReservationException;
+import ca.qc.collegeahuntsic.bibliotheque.exception.service.InvalidLoanLimitException;
+import ca.qc.collegeahuntsic.bibliotheque.exception.service.MissingLoanException;
+
 public interface IPretFacade extends IFacade {
-    //
+
+    void emprunter(Connexion connexion,
+        PretDTO pretDTO) throws InvalidHibernateSessionException,
+        InvalidDTOException,
+        InvalidPrimaryKeyException,
+        MissingDTOException,
+        InvalidCriterionException,
+        InvalidSortByPropertyException,
+        MissingLoanException,
+        ExistingLoanException,
+        ExistingReservationException,
+        InvalidLoanLimitException,
+        InvalidDTOClassException,
+        InvalidPrimaryKeyRequestException,
+        FacadeException;
+
+    void retourner(Connexion connexion,
+        PretDTO pretDTO) throws InvalidHibernateSessionException,
+        InvalidDTOException,
+        InvalidPrimaryKeyException,
+        MissingDTOException,
+        MissingLoanException,
+        InvalidDTOClassException,
+        FacadeException;
 }
