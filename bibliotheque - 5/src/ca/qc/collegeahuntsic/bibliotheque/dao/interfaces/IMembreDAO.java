7e4b8d68ba92ca4932d6d2451667374017f13795
Cedric_2 <Cedric_2@ASUSM11>
2014-10-16 21:57:26 -0500

    Corrections sur les commentaires.

--- bibliotheque - 5/src/ca/qc/collegeahuntsic/bibliotheque/dao/interfaces/IMembreDAO.java
+++ bibliotheque - 5/src/ca/qc/collegeahuntsic/bibliotheque/dao/interfaces/IMembreDAO.java
@@ -2,24 +2,29 @@
 package ca.qc.collegeahuntsic.bibliotheque.dao.interfaces;
 
 import java.util.List;
+import org.hibernate.Session;
 import ca.qc.collegeahuntsic.bibliotheque.dto.MembreDTO;
 import ca.qc.collegeahuntsic.bibliotheque.exception.dao.DAOException;
 import ca.qc.collegeahuntsic.bibliotheque.exception.dao.InvalidCriterionException;
 import ca.qc.collegeahuntsic.bibliotheque.exception.dao.InvalidCriterionValueException;
 import ca.qc.collegeahuntsic.bibliotheque.exception.dao.InvalidHibernateSessionException;
 import ca.qc.collegeahuntsic.bibliotheque.exception.dao.InvalidSortByPropertyException;
-import org.hibernate.Session;
+
+/**
+ * Interface DAO pour manipuler les membres dans la base de données.
+ *
+ */
 
 public interface IMembreDAO extends IDAO {
     /**
      * Trouve le membre. La liste est classée par ordre croissant sur <code>sortByPropertyName</code>. Si aucun membre
      * n'est trouvé, une {@link List} vide est retournée.
      *
-     * @param connexion La connexion à utiliser
+     * @param session La session Hibernate à utiliser
      * @param nom Le nom du membre à trouver
      * @param sortByPropertyName The nom de la propriété à utiliser pour classer
      * @return La liste des membre correspondants ; une liste vide sinon
-     * @throws InvalidHibernateSessionException Si la connexion est <code>null</code>
+     * @throws InvalidHibernateSessionException Si la session est <code>null</code>
      * @throws InvalidCriterionException Si l'ID du membre est <code>null</code>
      * @throws InvalidSortByPropertyException Si la propriété à utiliser pour classer est <code>null</code>
      * @throws DAOException S'il y a une erreur avec la base de données
@@ -34,15 +39,15 @@ public interface IMembreDAO extends IDAO {
         DAOException;
 
     /**
-     * Trouve les prêts d'un livre. La liste est classée par ordre croissant sur <code>sortByPropertyName</code>. Si aucun membre
+     * Trouve les membres à partir d'un numéro de téléphone. La liste est classée par ordre croissant sur <code>sortByPropertyName</code>. Si aucun membre
      * n'est trouvé, une {@link List} vide est retournée.
      *
-     * @param connexion La connexion à utiliser
+     * @param session La Hibernate à utiliser
      * @param numTel Le numéro de téléphone du membre à trouver
      * @param sortByPropertyName The nom de la propriété à utiliser pour classer
-     * @return La liste des prêts correspondant ; une liste vide sinon
-     * @throws InvalidHibernateSessionException Si la connexion est <code>null</code>
-     * @throws InvalidCriterionException Si l'ID du livre est <code>null</code>
+     * @return La liste des membres correspondant ; une liste vide sinon
+     * @throws InvalidHibernateSessionException Si la session est <code>null</code>
+     * @throws InvalidCriterionException Si l'ID du membre est <code>null</code>
      * @throws InvalidSortByPropertyException Si la propriété à utiliser pour classer est <code>null</code>
      * @throws DAOException S'il y a une erreur avec la base de données
      */
