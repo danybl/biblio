042c91c26dc12d8a7ba11ca95850bce4dcde67b0
JaskaranD <d.j.singh@hotmail.com>
2014-10-23 18:09:21 -0400

    1er cleanUp

--- bibliotheque - 6/src/ca/qc/collegeahuntsic/bibliothequeBackEnd/dao/interfaces/IReservationDAO.java
+++
@@ -1,9 +0,0 @@
-
-package ca.qc.collegeahuntsic.bibliothequeBackEnd.dao.interfaces;
-
-/**
- * Interface DAO pour manipuler les réservations dans la base de données.
- */
-public interface IReservationDAO extends IDAO {
-    //TODO
-}
