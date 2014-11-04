c99de56428acaf347e0b7efa902bcd5cc5eb0808
David Gallego <davidgallego0125@gmail.com>
2014-11-04 17:29:19 -0500

    ajout de package-info

---
+++ bibliotheque - 6 - back-end/src/ca/qc/collegeahuntsic/bibliothequeBackEnd/dto/package-info.java
@@ -0,0 +1,11 @@
+// Fichier package-info.java
+// Auteur : David Gallego
+// Date de création : 2014-11-04
+/**
+ * TODO Auto-generated class javadoc.
+ *
+ * @author David Gallego
+ */
+
+package ca.qc.collegeahuntsic.bibliothequeBackEnd.dto;
+
