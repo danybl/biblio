0c947f70291b527cc28daea911bfb916dd0521a5
JaskaranD <d.j.singh@hotmail.com>
2014-11-05 13:08:18 -0500

    correction checkstyle Exception facade

---
+++ bibliotheque - 6 - back-end/src/ca/qc/collegeahuntsic/bibliothequeBackEnd/exception/facade/package-info.java
@@ -0,0 +1,11 @@
+// Fichier package-info.java
+// Auteur : Cedric Soumpholphakdy, Dany Benoit-Lafond, Nkezimana Franz, Jaskaran Singh Dhadda & David Andrés Gallego Mesa
+// Date de création : 2014-11-04
+/**
+ * TODO Auto-generated class javadoc.
+ *
+ * @author Cedric Soumpholphakdy, Dany Benoit-Lafond, Nkezimana Franz, Jaskaran Singh Dhadda & David Andrés Gallego Mesa
+ */
+
+package ca.qc.collegeahuntsic.bibliothequeBackEnd.exception.facade;
+
