e461515acd77dd41278267609cfb3384c042d7a8
Dany Benoit Lafond <dany.benoit.lafond@gmail.com>
2014-11-16 15:25:15 -0500

    ajout

---
+++ bibliotheque - 7/src/ca/qc/collegeahuntsic/bibliotheque/filter/package-info.java
@@ -0,0 +1,10 @@
+// Fichier package-info.java
+// Date de création : 2014-11-16
+/**
+ * TODO Auto-generated class javadoc.
+ *
+ * @author Cedric Soumpholphakdy, Dany Benoit-Lafond, Nkezimana Franz, Jaskaran Singh Dhadda & David Andrés Gallego Mesa
+ */
+
+package ca.qc.collegeahuntsic.bibliotheque.filter;
+
