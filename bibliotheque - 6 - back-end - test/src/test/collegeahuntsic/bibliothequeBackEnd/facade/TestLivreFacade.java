0aebed2f54f2ca4849f1d38b4e1e3adb02ece8bc
JaskaranD <d.j.singh@hotmail.com>
2014-12-05 20:14:59 -0500

    testGetLivre() + javadoc

--- bibliotheque - 6 - back-end - test/src/test/collegeahuntsic/bibliothequeBackEnd/facade/TestLivreFacade.java
+++ bibliotheque - 6 - back-end - test/src/test/collegeahuntsic/bibliothequeBackEnd/facade/TestLivreFacade.java
@@ -1,8 +1,17 @@
 
 package test.collegeahuntsic.bibliothequeBackEnd.facade;
 
+import java.sql.Timestamp;
+import java.util.List;
 import junit.framework.Test;
 import junit.framework.TestSuite;
+import ca.qc.collegeahuntsic.bibliothequeBackEnd.dto.LivreDTO;
+import ca.qc.collegeahuntsic.bibliothequeBackEnd.exception.dao.InvalidHibernateSessionException;
+import ca.qc.collegeahuntsic.bibliothequeBackEnd.exception.dao.InvalidPrimaryKeyException;
+import ca.qc.collegeahuntsic.bibliothequeBackEnd.exception.dao.InvalidSortByPropertyException;
+import ca.qc.collegeahuntsic.bibliothequeBackEnd.exception.facade.FacadeException;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import test.collegeahuntsic.bibliothequeBackEnd.exception.TestCaseFailedException;
 
 /**
@@ -13,8 +22,8 @@ import test.collegeahuntsic.bibliothequeBackEnd.exception.TestCaseFailedExceptio
  */
 public class TestLivreFacade extends TestCase {
 
-    //    private final static Log LOGGER = LogFactory.getLog(TestLivreFacade.class);
-    //
+    private final static Log LOGGER = LogFactory.getLog(TestLivreFacade.class);
+
     private final static String TEST_CASE_TITLE = "Livre facade test case"; //$NON-NLS-1$
 
     //
@@ -58,4 +67,73 @@ public class TestLivreFacade extends TestCase {
         return suite;
     }
 
+    /**
+     * Tests if {@link ca.qc.collegeahuntsic.bibliothequeBackEnd.facade.interfaces.ILivreFacade#getLivre(org.hibernate.Session, String)}.
+     *
+     * @throws TestCaseFailedException If an error occurs
+     */
+
+    public void testGetLivre() throws TestCaseFailedException {
+        try {
+            testAcquerirLivre();
+
+            beginTransaction();
+            final List<LivreDTO> livres = getLivreFacade().getAllLivres(getSession(),
+                LivreDTO.TITRE_COLUMN_NAME);
+            assertFalse(livres.isEmpty());
+            LivreDTO livreDTO = livres.get(livres.size() - 1);
+            assertNotNull(livreDTO);
+            assertNotNull(livreDTO.getIdLivre());
+            assertNotNull(livreDTO.getTitre());
+            assertNotNull(livreDTO.getAuteur());
+            assertNotNull(livreDTO.getDateAcquisition());
+            final String idLivre = livreDTO.getIdLivre();
+            final String titre = livreDTO.getTitre();
+            final String auteur = livreDTO.getAuteur();
+            final Timestamp dateAcquisition = livreDTO.getDateAcquisition();
+
+            commitTransaction();
+
+            beginTransaction();
+
+            livreDTO = getLivreFacade().getLivre(getSession(),
+                idLivre);
+            assertNotNull(livreDTO);
+            assertNotNull(livreDTO.getIdLivre());
+            assertNotNull(livreDTO.getTitre());
+            assertNotNull(livreDTO.getAuteur());
+            assertNotNull(livreDTO.getDateAcquisition());
+
+            assertEquals(idLivre,
+                livreDTO.getIdLivre());
+            assertEquals(titre,
+                livreDTO.getTitre());
+            assertEquals(auteur,
+                livreDTO.getAuteur());
+            assertEquals(dateAcquisition,
+                livreDTO.getDateAcquisition());
+
+            commitTransaction();
+
+            beginTransaction();
+
+            livreDTO = getLivreFacade().getLivre(getSession(),
+                "-1");
+            assertNull(livreDTO);
+
+            commitTransaction();
+
+        } catch(
+            InvalidHibernateSessionException
+            | InvalidSortByPropertyException
+            | InvalidPrimaryKeyException
+            | FacadeException exception) {
+            try {
+                rollbackTransaction();
+            } catch(TestCaseFailedException testCaseFailedException) {
+                TestLivreFacade.LOGGER.error(testCaseFailedException);
+            }
+            TestLivreFacade.LOGGER.error(exception);
+        }
+    }
 }
